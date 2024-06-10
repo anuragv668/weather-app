669235f04054989be87eac7c590e0cfa5e6e2f18
anuragv668 <anuragvarma668@gmail.com>
2024-06-10 13:09:53 +0530

    make api requests

--- src/index.js
+++ src/index.js
@@ -1,2 +1,15 @@
 import './style.css';
 
+const key = "adcbf45248bf42e6a43140624240306";
+const locations = 'mumbai';
+
+
+let fetcher = async (locations) => {
+  const fetched = await fetch(`https://api.weatherapi.com/v1/current.json?key=${key}&q=${locations}`, {mode: 'cors'})
+  const response = await fetched.json();
+  const place = `${response.location.name}, ${response.location.country}`;
+  console.log(response);
+  console.log(place);
+}
+
+fetcher(locations);
