d72eb24ac077b705f3b654fa03387b287bec23d6
anuragv668 <anuragvarma668@gmail.com>
2024-06-10 14:10:32 +0530

    add logic for unit of temp

--- src/index.js
+++ src/index.js
@@ -1,15 +1,29 @@
 import './style.css';
 
 const key = "adcbf45248bf42e6a43140624240306";
-const locations = 'mumbai';
+const place = 'mumbai';
+let unit = 'celsius';
+
+const datetime = document.querySelector('.datetime');
+const location = document.querySelector('.location');
+const temperature = document.querySelector('.temperature');
 
 
-let fetcher = async (locations) => {
+let fetcher = async (unit, locations) => {
   const fetched = await fetch(`https://api.weatherapi.com/v1/current.json?key=${key}&q=${locations}`, {mode: 'cors'})
   const response = await fetched.json();
   const place = `${response.location.name}, ${response.location.country}`;
-  console.log(response);
-  console.log(place);
+  const tempc = `${response.current.temp_c}`;
+  const tempf = `${response.current.temp_c}`;
+  const time = `${response.current.last_updated}`;
+
+  datetime.textContent = time;
+  location.textContent = place;
+  if (unit === 'celsius') {
+    temperature.textContent = `${tempc}°C`;
+  } else {
+  temperature.textContent = `${tempc}°F`
+  }
 }
 
-fetcher(locations);
+fetcher(unit, place);
